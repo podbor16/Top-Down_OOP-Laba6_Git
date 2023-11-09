5dc91732753b8e113758cce45170a626185ff243
podbor16 <podbor250718@gmail.com>
2023-11-09 14:12:49 +0700

    Create classes

--- laba1_oop.py
+++ laba1_oop.py
@@ -1,20 +1,74 @@
 class Worker:
-    def __init__(self):
-        return
+    def __init__(self, name, surname, age):
+        self.__name = name
+        self.__surname = surname
+        self.__age = age
+
+    @property
+    def name(self):
+        return self.__name
+
+    @property
+    def surname(self):
+        return self.__surname
+
+    @property
+    def age(self):
+        return self.__age
 
     def __str__(self):
-        return
+        return f"{self.name} {self.surname}, Возраст: {self.age}"
+
 
 class Director:
-    def __init__(self):
-        return
+    def __init__(self, name, surname, age):
+        self.__name = name
+        self.__surname = surname
+        self.__age = age
+
+    @property
+    def name(self):
+        return self.__name
+
+    @property
+    def surname(self):
+        return self.__surname
+
+    @property
+    def age(self):
+        return self.__age
 
     def __str__(self):
-        return
+        return f"{self.name} {self.surname}, Возраст: {self.age}"
 
 class Firm:
-    def __init__(self):
-        return
+    def __init__(self, name_of_firm):
+        self.__name_of_firm = name_of_firm
+        self.__director = None
+        self.__workers = []
+
+    @property
+    def name_of_firm(self):
+        return self.__name_of_firm
+
+    @property
+    def director(self):
+        return self.__director
+
+    @property
+    def workers(self):
+        return self.__workers
+
+    def make_director(self, director):
+        self.__director = director
+
+    def make_worker(self, worker):
+        self.__workers.append(worker)
+
+    def __str__(self):
+        director_str = f"Директор: {self.director}" if self.director else "Директор не назначен"
+        workers_str = "\n".join(str(worker) for worker in self.workers)
+        return f"Фирма: {self.name_of_firm}\n{director_str}\nСотрудники:\n{workers_str}"
 
 def create_firm():
     return
