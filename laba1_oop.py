538e5d6fefa71cd2c41d2f40b985ea156651c6ce
podbor16 <podbor250718@gmail.com>
2023-11-09 14:02:52 +0700

    The First stage

---
+++ laba1_oop.py
@@ -0,0 +1,60 @@
+class Worker:
+    def __init__(self):
+        return
+
+    def __str__(self):
+        return
+
+class Director:
+    def __init__(self):
+        return
+
+    def __str__(self):
+        return
+
+class Firm:
+    def __init__(self):
+        return
+
+def create_firm():
+    return
+
+def create_director():
+    return
+
+def create_worker():
+    return
+
+def menu():
+    firm = None
+    director = None
+
+    while True:
+        print("Главное меню:")
+        print("1. Создать фирму.")
+        print("2. Создать директора(количество - 1).")
+        print("3. Создать сотрудника.(количество - не ограничено)")
+        print("4. Вывести информацию о сотруднике.")
+        print("5. Вывести информацию о директоре.")
+        print("6. Вывести информацию о фирме.")
+        print("7. Выход из программы ")
+
+        choose = input("Выберите пункт меню: ")
+
+        if choose == "1":
+            pass
+        elif choose == "2":
+            pass
+        elif choose == "3":
+            pass
+        elif choose == "4":
+            pass
+        elif choose == "5":
+            pass
+        elif choose == "6":
+            pass
+        elif choose == "7":
+            pass
+
+if __name__ == "__main__":
+    menu()
