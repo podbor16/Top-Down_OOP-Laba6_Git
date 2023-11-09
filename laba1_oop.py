0e1beba54deb0d5e91987018eff3ff0eed4fe2fa
podbor16 <podbor250718@gmail.com>
2023-11-09 14:17:12 +0700

    Create functions

--- laba1_oop.py
+++ laba1_oop.py
@@ -19,7 +19,6 @@ class Worker:
     def __str__(self):
         return f"{self.name} {self.surname}, Возраст: {self.age}"
 
-
 class Director:
     def __init__(self, name, surname, age):
         self.__name = name
@@ -71,13 +70,46 @@ class Firm:
         return f"Фирма: {self.name_of_firm}\n{director_str}\nСотрудники:\n{workers_str}"
 
 def create_firm():
-    return
+    name_of_firm = input("Введите название фирмы: ")
+    return Firm(name_of_firm)
 
 def create_director():
-    return
+    while True:
+        name = input("Введите имя директора: ")
+        if name.isalpha():
+            break
+        else:
+            print("Ошибка. Имя должно состоять из букв. Введите корректное имя. ")
+    surname = input("Введите фамилию директора: ")
+
+    while True:
+        age = input("Введите возраст директора: ")
+        try:
+            age = int(age)
+            break  # Если возраст успешно преобразован в целое число, завершаем цикл
+        except ValueError:
+            print("Ошибка: Возраст должен быть числом. Попробуйте снова.")
+
+    return Director(name, surname, age)
 
 def create_worker():
-    return
+    while True:
+        name = input("Введите имя сотрудника: ")
+        if name.isalpha():
+            break
+        else:
+            print("Ошибка. Имя должно состоять из букв. Введите корректное имя. ")
+    surname = input("Введите фамилию сотрудника: ")
+
+    while True:
+        age = input("Введите возраст сотрудника: ")
+        try:
+            age = int(age)
+            break  # Если возраст успешно преобразован в целое число, завершаем цикл
+        except ValueError:
+            print("Ошибка: Возраст должен быть числом. Попробуйте снова.")
+
+    return Worker(name, surname, age)
 
 def menu():
     firm = None
